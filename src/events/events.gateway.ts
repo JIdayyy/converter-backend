41404c47f37157d08d1d756f5c34609f9aef719e
Julien Abbadie <julien.abbadie@pytheascapital.com>
2024-03-07 11:50:56 +0100

    feat: socket handling

---
+++ src/events/events.gateway.ts
@@ -0,0 +1,52 @@
+import {
+  WebSocketGateway,
+  WebSocketServer,
+  SubscribeMessage,
+  OnGatewayInit,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+import { EventsService } from './events.service';
+
+@WebSocketGateway(3004, {
+  cors: {
+    origin: 'http://localhost:3000',
+  },
+})
+export class EventsGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  private readonly logger = console;
+
+  constructor(private readonly eventsService: EventsService) {}
+
+  @WebSocketServer() io: Server;
+
+  onModuleInit() {
+    this.logger.log('Module initialized');
+  }
+
+  afterInit() {
+    this.logger.log('Initialized');
+  }
+
+  handleConnection(socket: Socket) {
+    this.logger.log(`Client id:${socket.id} connected`);
+    this.eventsService.handleConnection(socket);
+  }
+
+  handleDisconnect(client: any) {
+    this.logger.log(`Cliend id:${client.id} disconnected`);
+  }
+
+  @SubscribeMessage('ping')
+  handleMessage(client: any, data: any) {
+    this.logger.log(`Message received from client id: ${client.id}`);
+    this.logger.debug(`Payload: ${data}`);
+    return {
+      event: 'pong',
+      data: 'Wrong data that will make the test fail',
+    };
+  }
+}
